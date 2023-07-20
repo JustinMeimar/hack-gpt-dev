131b187c771129557fd4a4d4502d75781a252ca9
Justin Meimar <j.meimar111@gmail.com>
2023-07-20 09:53:14 -0600

    add llm dir

---
+++ app/api/llm/mock_llm.py
@@ -0,0 +1,9 @@
+"""
+mock prompt processing to show where to handle
+
+"""
+def process_prompt(prompt):
+    if prompt:
+        return {
+            "response": "response to prompt"
+        }
--- app/api/views.py
+++ app/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from api.llm.mock_llm import process_prompt
 
 @api_view(['POST'])
 def prompt_endpoint(request):
@@ -8,6 +9,7 @@ def prompt_endpoint(request):
     prompt = request.query_params.get('prompt', None)
 
     if prompt: 
-        return Response({'prompt': prompt})
+        response = process_prompt(prompt) 
+        return Response(response)
     else: 
         return Response({'error': 'prompt was not receieved or did not process'})
